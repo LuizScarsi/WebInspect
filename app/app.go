feb51c13b025ee596db710697e48cb81a050adc1
luizscarsi <luizaugustoscarsi@gmail.com>
2023-12-21 14:15:29 -0300

    feat: Add ip command

--- app/app.go
+++ app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Returns the CLI App ready to be executed
 func Generate() *cli.App {
@@ -8,5 +14,33 @@ func Generate() *cli.App {
 	app.Name = "WebInspect"
 	app.Usage = "Searches for a website IP address and server name"
 	
+	app.Commands = []cli.Command{
+		{
+			Name: "ip",
+			Usage: "Search IP address",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name: "host",
+					Value: "google.com.br",
+				},
+			},
+			Action: searchIps,
+
+		},
+	}
+
 	return app
+}
+
+func searchIps(c *cli.Context) {
+	host := c.String("host")
+	
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
